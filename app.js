5e18d15da0bdec4fc49977842c7cada4da5c45a7
tejalshah84 <tejalshah84@gmail.com>
2015-08-22 15:38:54 -0700

    With DB without auth

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Import modules
 var express = require('express'), 
     http = require('http'), 
     path = require('path'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    sqlite3 = require('sqlite3').verbose();
 
+//Create express instance
 var app = express();
 
+//Configure middleware stack and define paths
 app.configure(function() {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
@@ -21,49 +25,206 @@ app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
-var postHistory = [{userName: 'Tejal', timeStamp: new Date().getTime(), message: 'hey There'},
-                    {userName: 'Swami', timeStamp: new Date().getTime(), message: 'I love you'} ];
+//Database initializiation
+var db = new sqlite3.Database('fsechat.db');
 
-// Set up express
+db.serialize(function (){
+
+    //db.run("DROP Table if EXISTS user_profile", function(err){});
+    //db.run("DROP Table if EXISTS chat_history", function(err){});
+
+    // Create Tables if they don't exist
+    db.run("CREATE Table if NOT EXISTS user_profile (userid INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(25) UNIQUE NOT NULL, password VARCHAR(25) NOT NULL, firstname VARCHAR(25), lastname VARCHAR(25))", function (err){
+
+        if(err!==null){
+            console.log("Error occured while creating user_profile table");
+        }
+        else{
+            console.log("user_profile table initialized");
+        }
+    });
+
+    db.run("CREATE Table if NOT EXISTS chat_history (chatid INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(25), timestamp DATETIME, chatmessage BLOB)", function (err){
+
+        if(err!==null){
+            console.log("Error occured while creating chat_history table");
+        }
+        else{
+            console.log("chat_history table initialized");
+        }
+    });
+
+
+    db.each("SELECT * from user_profile", function (err, row){
+        if (err){
+            console.log('not retrieving data');
+        }
+        else{
+            console.log(row);
+        }
+    });
+
+    db.each("SELECT * from chat_history", function (err, row){
+        if (err){
+            console.log('not retrieving data');
+        }
+        else{
+            console.log(row);
+        }
+    });
+
+});
+
+
+
+// Setup http server
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
 
+//Handle webpage routing
 app.get('/', function (request, response){
     response.redirect('login.html');
 });
 
 app.post('/login', function (request, response){
-    //auth
-    response.redirect('/chatRoom');
+    
+    //Retrieve matching login credentials from DB
+    db.get("SELECT username, password from user_profile where username=$username", 
+        {$username: request.body.userName}, function (err, row){
+       
+        if (err){ 
+            response.render('login', {pageMessage: "Unable to login, please try again"});
+            console.log("Unable to login, please try again");
+        }
+        else if (row===undefined){
+            response.render('login', {pageMessage: "This Username does not exist, please use new user flow to create ID"});
+            console.log("User does not exist");
+        }
+        else if (row.username!==request.body.userName || row.password!==request.body.password){
+            response.render('login', {pageMessage: "Incorrect Username or Password, please try again"});
+            console.log("Incorrect credentials");
+        }
+        else {
+         console.log(row);  
+         response.redirect('/chatRoom');
+        }   
+        
+    });
 });
 
 app.post('/createUser', function (request, response){
-    //auth
-    response.redirect('/chatRoom');
+    
+    db.serialize( function(){
+
+        //Check if username already exists
+        db.get("SELECT username from user_profile where username=$username", 
+        {$username: request.body.userName}, function (err, row){
+
+            if (err){
+                response.render('createUser', {pageMessage: "Unable to create ID, please try again"});
+                console.log("Unable to create ID, please try again");
+            }
+            else if (row===undefined){
+
+                db.serialize( function(){
+
+                    //Create New User record in DB
+                    db.run("INSERT INTO user_profile VALUES($next_id, $username, $password, $firstname, $lastname)", {
+                        $username: request.body.userName,
+                        $password: request.body.password,
+                        $firstname: request.body.firstName,
+                        $lastname: request.body.lastName
+                    }, function (err){
+
+                        if (err){
+                          response.render('createUser', {pageMessage: "Error while creating user, try again"});
+                            
+                        }
+                        else {
+                          console.log('User created')
+                          response.redirect('/chatRoom');  
+                        }
+                    });
+    
+                });
+
+            }
+            else {
+                response.render('createUser', {pageMessage: "User ID already exists, use alternate username or proceed to login page."});
+                console.log("Login ID already exists");
+            }
+
+        });
+    }); 
 });
 
 app.get('/chatRoom', function (request, response){
-    //auth
-    console.log(postHistory);
-    response.render('chatroom', {oldposts: postHistory});
+
+    var postHistory = [];
+    var postHist = {};
+
+    db.serialize( function(){
+
+        db.all("SELECT * from chat_history", function (err, rows){
+            if (err){
+                console.log('not retrieving data');
+            }
+            else{
+                console.log('loading chat_history');
+                rows.forEach(function(row){
+                    postHist = {};
+                    postHist.userName= row.username,
+                    postHist.timeStamp= row.timestamp,
+                    postHist.message= row.chatmessage
+
+                
+                    console.log(postHist);
+                    postHistory.push(postHist);
+                });
+                
+                console.log(postHistory);
+                response.render('chatroom', {oldposts: postHistory});
+            }
+
+        });
+
+    });
+    
 });
 
 
 
-// Set up socket.io
+// Set up socket.io listener
 var io = require('socket.io').listen(server);
 
 // Handle socket traffic
 io.sockets.on('connection', function (socket) {
+    
     // Relay chat data to all clients
     socket.on('chat', function(data) {
         
-        var oldpost = _.extend({userName: 'Keshav', timeStamp: new Date().getTime()}, data);
-        postHistory.push(oldpost); // store in DB
-        io.sockets.emit('chat', oldpost);
-        //socket.emit('chat',data);
-        //socket.broadcast.emit('chat', data);
+        var oldpost = _.extend({userName: 'Tejal', timeStamp: new Date().getTime()}, data);
+        
+        db.serialize( function(){
+
+            db.run("INSERT INTO chat_history VALUES($next_id, $username, $timestamp, $chatmessage)", {
+                        $username: oldpost.userName,
+                        $timestamp: oldpost.timeStamp,
+                        $chatmessage: oldpost.message
+                    }, function (err){
+
+                        if(err){
+                            console.log('Error occured while storing in chat history');
+                        }
+                        else{
+                          io.sockets.emit('chat', oldpost);  
+                        }
+                    }
+            );
+
+        });
+        
     });
 
     socket.on('enterRoom', function(name){
@@ -71,6 +232,6 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('disconnect', function(){
-       console.log('disconnected');
+       console.log('User disconnected');
     });
 });
